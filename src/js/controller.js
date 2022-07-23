2f11c8c432d4f8e1b2e360fcdca4c17658eb6d85
vonryanpogi <vonj.manalansan@gmail.com>
2022-07-23 18:45:53 +0800

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -123,6 +123,10 @@ const controlAddRecipe = async function (newRecipe) {
   // Upload the new recipe data
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -131,5 +135,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
